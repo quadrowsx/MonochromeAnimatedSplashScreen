4f116da491e731ccbd8e57ae89905ab7cac7ba7e
Gulov Alimuhammad <qwertyedcrfvgulov@gmail.com>
2023-09-17 17:20:57 +0500

    FirstCommit

---
+++ app/src/main/java/com/anime_wallpaper/top/checkmonochrome/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.anime_wallpaper.top.checkmonochrome
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
